8c851e027dc53c1ecf936cf670e89d1c301061bf
abe <alsoraimi46@gmail.com>
2017-03-23 17:30:43 -0400

    Enter code to output reverse number

--- pre work grand circ/pre work grand circ/Program.cs
+++ pre work grand circ/pre work grand circ/Program.cs
@@ -10,18 +10,19 @@ namespace pre_work_grand_circ
     {
         static void Main(string[] args)
         {
-            int num = 0;
-            int revNum = 0;
+            int num = 0;               
+            int revNum = 0;       // named revNum for reversed number
             Console.WriteLine("please enter any number higher than 0: ");
             num = int.Parse(Console.ReadLine());  // had to convert from int to string
             while (num != 0) ;
             {
-                
-                
-
-
+                revNum = revNum * 10;   
+                revNum = revNum + num % 10; // added reversed number multiplied by 10 plus the remainder of original num and 10
+                num = num / 10;  // since this is a int there will be no decimles, which will in turn give you the reversed number
             }
 
+            Console.WriteLine("Your reversed outcome is {0}! " , revNum);
+            Console.ReadLine();
 
 
 
